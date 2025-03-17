73be08818f8d2395bd78b445fb5af3bdd1f63774
Jh3fferson <jheffersonmatias123@gmail.com>
2025-03-17 16:37:47 -0300

    Melhora no front-end

--- js/script.js
+++ js/script.js
@@ -1,52 +1,62 @@
 const form = document.querySelector("#form");
 const peso = document.getElementById("peso");
 const altura = document.getElementById("altura");
-const imc = document.getElementById("imc");
+const imc = document.getElementById("imcText");
 
 
 form.addEventListener("submit", function (event) {
-event.preventDefault();
+  event.preventDefault();
 
-  let imc = peso.value / ((altura.value/100) * (altura.value/100));
+  let imcValue = peso.value / ((altura.value / 100) * (altura.value / 100));
   const imcLabel = document.querySelector('#imc');
-  let imcText;
-  let color;
 
-  if (imc < 18.5) {
-    imcText = (`Seu IMC é de ${imc.toFixed(2)} Kg/m2. Você está abaixo do peso!`);
+  let imcText = selectImcText(imcValue);
+  document.querySelector('#imc').style.display = "flex";
+  document.querySelector('#button').style.display = "none";
+  moverSeta(imcValue);
 
-  } else if (imc < 25) {
-    imcText = (`Seu IMC é de ${imc.toFixed(2)} Kg/m2. Você está com o peso ideal!`);
-    
-  } else if (imc < 30) {
-    imcText = (`Seu IMC é de ${imc.toFixed(2)} Kg/m2. Você está com sobrepeso!`);
-    
-  } else if (imc < 35) {
-    imcText = (`Seu IMC é de ${imc.toFixed(2)} Kg/m2. Você está com obesidade grau I!`);
-    
-  } else if (imc < 40) {
-    imcText = (`Seu IMC é de ${imc.toFixed(2)} Kg/m2. Você está com obesidade grau II!`);
-
-  } else {
-    imcText = (`Seu IMC é de ${imc.toFixed(2)} Kg/m2. Você está com obesidade grau III!`);
-    
-  }
-  document.querySelector('#imc-div').style.display="flex";
-  document.querySelector('#imc-div').style.backgroundColor=color;
-
-  moverSeta(imc);
-
-  imcLabel.textContent = imcText;
+  imc.textContent = imcText;
 });
 
+function selectImcText(imcValue) {
+  let imcText;
 
-function moverSeta(imc) {
+  if (imcValue < 18.5) {
+    imcText = (`Seu IMC é de ${imcValue.toFixed(2)} Kg/m2. Você está abaixo do peso!`);
 
-    document.querySelector('.barra').style.display = "block";
-   
-    imc = (imc > 50) ? 50 : imc;
-    imc = (imc < 1) ? 1 : imc;
-    const larguraBarra = document.querySelector('.barra').offsetWidth;
-    const posicao = (imc-1) * (larguraBarra/50);
-    document.querySelector('.seta').style.left = `${posicao}px`;
+  } else if (imcValue < 25) {
+    imcText = (`Seu IMC é de ${imcValue.toFixed(2)} Kg/m2. Você está com o peso ideal!`);
+
+  } else if (imcValue < 30) {
+    imcText = (`Seu IMC é de ${imcValue.toFixed(2)} Kg/m2. Você está com sobrepeso!`);
+
+  } else if (imcValue < 35) {
+    imcText = (`Seu IMC é de ${imcValue.toFixed(2)} Kg/m2. Você está com obesidade grau I!`);
+
+  } else if (imcValue < 40) {
+    imcText = (`Seu IMC é de ${imcValue.toFixed(2)} Kg/m2. Você está com obesidade grau II!`);
+
+  } else {
+    imcText = (`Seu IMC é de ${imcValue.toFixed(2)} Kg/m2. Você está com obesidade grau III!`);
+
+  }
+
+  return imcText;
+}
+
+
+function moverSeta(imcValue) {
+
+  imcValue = (imcValue > 50) ? 50 : imcValue;
+  imcValue = (imcValue < 1) ? 1 : imcValue;
+  const larguraBarra = document.querySelector('.barra').offsetWidth;
+  const posicao = (imcValue - 1) * (larguraBarra / 50);
+  document.querySelector('.seta').style.left = `${posicao}px`;
+}
+
+function closeImc(){
+  document.querySelector('#imc').style.display = "none";
+  document.querySelector('#button').style.display = "flex";
+  peso.value = "";
+  altura.value = "";
 }
